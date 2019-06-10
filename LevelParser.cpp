a555a7995339ff0ce812a74c3f7e4fa961f08c03
Shaun Hose <hoseshaun@gmail.com>
2019-06-10 22:28:59 +1000

    new LevelParser comms

--- LevelParser.cpp
+++ LevelParser.cpp
@@ -13,7 +13,7 @@
 Level* LevelParser::parseLevel(const char *levelFile)
 {
   //Create parameters class to send all parameters from data file paragraph into any desired class when creating.
-
+  //New plans and comments here.
     
     // create a tinyXML document and load the map xml
     TiXmlDocument levelDocument;
