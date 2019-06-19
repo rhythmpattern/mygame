6dba863fbc4372b6d0bc41be67cb79b797e2295b
Shaun Hose <hoseshaun@gmail.com>
2019-06-19 17:37:32 +1000

    removed comments to fix time

--- ProjectileManager.cpp
+++ ProjectileManager.cpp
@@ -63,7 +63,7 @@ void ProjectileManager::AddShot(Character* shooter, Vector2D target)
   Projectile* rp = new Projectile_Shot(shooter, target);
 
   m_Projectiles.push_back(rp);
-  std::cout << "Projectiles size is: " << m_Projectiles.size() << '\n';  
+ 
   #ifdef LOG
   // std::cout << "Adding a shot " << rp->ID() << " at pos " << rp->Pos() << "\n";
   #endif
